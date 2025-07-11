ec78aeb3ba19e8b046ca6604ac67a3413e768389
Fazal2307-dev <mdfazal2307@gmail.com>
2025-07-11 19:19:59 +0530

    cont

---
+++ src/Components/ContactUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactUs() {
+  return (
+    <div>
+      <h2>You can contact us for work or open source contribution </h2>
+    </div>
+  )
+}
+
+export default ContactUs;
