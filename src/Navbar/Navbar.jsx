9347c0199f49d82cb8486b3145fe2c0b9033d520
Fazal2307-dev <mdfazal2307@gmail.com>
2025-07-11 19:37:33 +0530

    navbar

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function Navbar() {
+  return (
+    <div className='bg-gray-600 text-white'>
+      <ul className='flex justify-between p-5' >
+        <li>Home</li>
+        <li>POW</li>
+        <li>Contact US</li>
+        <li> Resume</li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
